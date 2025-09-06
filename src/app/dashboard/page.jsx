0ca43ce4c8bef8ba06db5af2c65ef4c6b9f3e147
Eliel J. E. Escoto <77648539+ElielJEE@users.noreply.github.com>
2025-09-06 00:25:38 -0600

    ADD: dashboard ui complete i suppose

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,10 +2,10 @@ import { Button, InfoCard } from '@/components/atoms'
 import React from 'react'
 import { FiDollarSign } from "react-icons/fi";
 import { BsCart2, BsBoxSeam, BsEye } from "react-icons/bs";
-import { HiOutlineUserGroup } from "react-icons/hi2";
+import { HiArrowTrendingUp, HiOutlineUserGroup } from "react-icons/hi2";
 import { CiWarning } from "react-icons/ci";
 import { HiOutlineCalculator } from "react-icons/hi";
-
+import { IoArrowForwardOutline } from "react-icons/io5";
 
 export default function Dashboard() {
 	const ventasData = [
@@ -24,6 +24,13 @@ export default function Dashboard() {
 		{ product: 'Producto C', amount: 1000, count: 70 },
 		{ product: 'Producto D', amount: 800, count: 530 },
 		{ product: 'Producto E', amount: 600, count: 350 },
+	];
+
+	const StockAlerts = [
+		{ product: 'Producto X', stock: 5, maxStock: 10 },
+		{ product: 'Producto Y', stock: 2, maxStock: 30 },
+		{ product: 'Producto Z', stock: 0, maxStock: 20 },
+		{ product: 'Producto A', stock: 8, maxStock: 50 },
 	];
 
 	const maxAmount = Math.max(...ventasData.map(data => data.amount));
@@ -113,6 +120,99 @@ export default function Dashboard() {
 							</div >
 						))
 					}
+				</div>
+			</section>
+			<section className='w-full mt-6 flex gap-4'>
+				<div className='flex w-[50%] border border-dark/20 rounded-lg flex-col gap-4 p-4'>
+					<div className='flex justify-between items-center'>
+						<div>
+							<div className='flex items-center gap-1'>
+								<CiWarning className='h-9 w-9 text-yellow' />
+								<h2 className='text-2xl font-bold text-dark'>Alertas de Stock</h2>
+							</div>
+							<span className='text-medium text-dark/50'>Productos que necesitan reabastecimiento</span>
+						</div>
+						<div>
+							<Button
+								className={"transparent"}
+								text={"Ver todo"}
+								iconRight={<IoArrowForwardOutline className='h-5 w-5' />}
+							/>
+						</div>
+					</div>
+					<div className='w-full flex flex-col gap-2 justify-center items-center text-dark/50'>
+						{StockAlerts.length > 0 ? (
+							StockAlerts.map((item, index) => (
+								item.stock <= (item.maxStock * 0.3) && (
+									<div key={index} className='w-full flex justify-center items-center border rounded-lg border-dark/20 p-2'>
+										<div className='w-full flex flex-col'>
+											<h2 className='text-dark text-lg font-semibold mb-2'>{item.product}</h2>
+											<div className='w-[570px] flex'>
+												<div className='w-full bg-secondary rounded-full h-2'>
+													<div className='bg-dark h-2 rounded-full' style={{ width: `${(item.stock / item.maxStock) * 100}%` }}></div>
+												</div>
+												<p className='text-dark/70 text-sm'>{item.stock}/{item.maxStock}</p>
+											</div>
+										</div>
+										<span className='bg-secondary text-light p-4 text-center text-sm rounded-full flex justify-center items-center h-5'>
+											{
+												item.stock === 0
+													? "Agotado"
+													: item.stock <= (item.maxStock * 0.3)
+													&& "bajo"
+											}
+										</span>
+									</div>
+								)
+
+							))
+						) :
+							<p>No hay alertas de stock</p>
+						}
+					</div>
+				</div>
+				<div className='flex w-[50%] border border-dark/20 rounded-lg flex-col gap-4 p-4'>
+					<div>
+						<h2 className='text-2xl font-bold text-dark'>Actividad Reciente</h2>
+						<span className='text-medium text-dark/50'>Ultimas acciones realizadas en el sistema</span>
+					</div>
+					<div className='w-full h-48 flex justify-center items-center text-dark/50'>
+						<p>No hay actividad reciente</p>
+					</div>
+				</div>
+			</section>
+			<section className='w-full mt-6 flex border border-dark/20 rounded-lg flex-col p-4'>
+				<h2 className='text-dark text-xl font-bold'>Resumen del Mes</h2>
+				<span className='text-dark/50 text-medium'>Septiembre 2025</span>
+				<div className='w-full flex justify-around items-center mt-4'>
+					<div className='text-center flex flex-col justify-center items-center'>
+						<h2 className='text-xl font-bold text-success'>C$43000</h2>
+						<span className='text-medium text-dark/50'>Ingresos Totales</span>
+						<span className='text-success flex items-center gap-1'>
+							<HiArrowTrendingUp className='text-success' /> +15.6%
+						</span>
+					</div>
+					<div className='text-center flex flex-col justify-center items-center'>
+						<h2 className='text-xl font-bold text-primary'>3000</h2>
+						<span className='text-medium text-dark/50'>Productos Vendidos</span>
+						<span className='text-success flex items-center gap-1'>
+							<HiArrowTrendingUp className='text-success' /> +5.6%
+						</span>
+					</div>
+					<div className='text-center flex flex-col justify-center items-center'>
+						<h2 className='text-xl font-bold text-blue'>300</h2>
+						<span className='text-medium text-dark/50'>Clientes Unicos</span>
+						<span className='text-success flex items-center gap-1'>
+							<HiArrowTrendingUp className='text-success' /> +1%
+						</span>
+					</div>
+					<div className='text-center flex flex-col justify-center items-center'>
+						<h2 className='text-xl font-bold text-purple-500'>100%</h2>
+						<span className='text-medium text-dark/50'>Satisfaccion</span>
+						<span className='text-success flex items-center gap-1'>
+							<HiArrowTrendingUp className='text-success' /> +6%
+						</span>
+					</div>
 				</div>
 			</section>
 		</div>
--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Button({ func, text, type, className, icon }) {
+export default function Button({ func, text, type, className, icon, iconRight }) {
 	const ColorVariants = {
 		primary: 'bg-primary hover:bg-primary/80 text-light',
 		success: 'bg-success hover:bg-success/80 text-light',
@@ -10,12 +10,12 @@ export default function Button({ func, text, type, className, icon }) {
 		blue: 'bg-blue hover:bg-danger/80 text-light',
 		yellow: 'bg-yellow hover:bg-danger/80 text-light',
 		secondary: 'bg-secondary hover:bg-secondary/80 text-light',
-		transparent: 'bg-transparent hover:bg-primary text-dark hover:text-light border border-dark/20'
+		transparent: 'bg-transparent hover:bg-primary text-dark hover:text-light border border-dark/20 font-semibold',
 	}
 
 	return (
 		<button className={`btn ${ColorVariants[className]}`} onClick={func} type={type}>
-			{icon} {text}
+			{icon} {text} {iconRight}
 		</button>
 	)
 }
