e2fb608d0493598b2cb70d9fe537021e8c44b40c
Eliel J. E. Escoto <77648539+ElielJEE@users.noreply.github.com>
2025-12-02 17:11:52 -0600

    deploy test 2

--- src/lib/db.jsx
+++ src/lib/db.jsx
@@ -1,11 +1,20 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-export const pool = mysql.createPool({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_NAME || 'ferreteria_maytro',
-	port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
-});
+let pool;
 
-export default pool; // 👈 importante
+if (!global.pool) {
+  global.pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
+
+pool = global.pool;
+
+export default pool;
