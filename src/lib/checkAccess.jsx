ccebc97364972c97b3c917ec9dd71018104e1dc9
Eliel J. E. Escoto <77648539+ElielJEE@users.noreply.github.com>
2025-12-04 10:58:15 -0600

    deploy test 11

--- src/lib/checkAccess.jsx
+++ src/lib/checkAccess.jsx
@@ -1,33 +1,25 @@
-import { NextResponse } from "next/server";
 import { pool } from "@/lib/db";
 
-export async function checkAccess(req) {
-    try {
-        const { roleId, path } = await req.json();
+export async function checkAccess({ roleId, path }) {
+  try {
+    if (!roleId || !path) {
+      return { allowed: false };
+    }
 
-        if (!roleId || !path) {
-            return NextResponse.json(
-                { allowed: false, message: "Datos incompletos" },
-                { status: 400 }
-            );
-        }
-
-        const [rows] = await pool.query(
-            `
+    const [rows] = await pool.query(
+      `
       SELECT p.path
       FROM rol_permisos rp
       INNER JOIN permisos p ON rp.permiso_id = p.idpermisos
       WHERE rp.rol_id = ? AND ? LIKE CONCAT(p.path, '%')
       LIMIT 1
       `,
-            [roleId, path]
-        );
+      [roleId, path]
+    );
 
-        const allowed = rows.length > 0;
-
-        return NextResponse.json({ allowed });
-    } catch (error) {
-        console.error("Error en check-access:", error);
-        return NextResponse.json({ allowed: false }, { status: 500 });
-    }
+    return { allowed: rows.length > 0 };
+  } catch (error) {
+    console.error("Error en check-access:", error);
+    return { allowed: false };
+  }
 }
