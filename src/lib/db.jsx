930d9ade5c3ce1250f68c02e0f92bd6c344efda3
Eliel J. E. Escoto <77648539+ElielJEE@users.noreply.github.com>
2025-12-04 10:10:10 -0600

    deploy test 7

--- src/lib/db.jsx
+++ src/lib/db.jsx
@@ -8,7 +8,10 @@ export const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false, // ⚠️ esto es clave en Railway
+  },
 });
 
 export default pool;
